f85bc7776e3260ae8768fd806d0935224dd0e8a2
vishwajeet-00010 <singh.07vishwajeet@gmail.com>
2025-11-19 20:00:18 +0530

    inverted half pyramid pattern with numbers

---
+++ pattern2.java
@@ -0,0 +1,12 @@
+public class pattern2{
+    public static void inverted_half_pyramid_withno(int n){
+        for(int i=1;i<=n;i++){
+            for (int j=1;j<=(n-i+1);j++){
+                System.out.print(j+" ");
+            }System.out.println();
+        }
+    }
+    public static void main(String args[]){
+        inverted_half_pyramid_withno(5);
+    }
+}

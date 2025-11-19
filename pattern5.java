c3894ea0eef3888afbcca76c621b29a627f5b4d6
vishwajeet-00010 <singh.07vishwajeet@gmail.com>
2025-11-19 21:04:05 +0530

    butterfly pattern

---
+++ pattern5.java
@@ -0,0 +1,35 @@
+public class pattern5 {
+    public static void butterfly_pattern(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            for(int j=1;j<=2*(n-i);j++){
+                System.out.print(" ");
+
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+        
+            }  System.out.println(); 
+        }
+         for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            for(int j=1;j<=2*(n-i);j++){
+                System.out.print(" ");
+
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        butterfly_pattern(5);
+    }
+     
+
+}
+
